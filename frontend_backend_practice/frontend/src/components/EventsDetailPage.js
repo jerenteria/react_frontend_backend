4d88aee4f2a236beca6e99cc1445a67e737f8f94
jerenteria <renteria.juan96@gmail.com>
2024-10-28 19:52:00 -0700

    rendered the event details on every event

--- frontend_backend_practice/frontend/src/components/EventsDetailPage.js
+++ frontend_backend_practice/frontend/src/components/EventsDetailPage.js
@@ -1,11 +1,33 @@
 import { useParams } from "react-router-dom";
+import { EVENTS } from "./EventsPage.js";
 
 function EventsDetailPage() {
+// useParams() is a hook provided by React Router that extracts parameters from the current URL 
+// grabs the info from the event being clicked on
+
+    // if URL is path: '/events/1', useParams will grab the info from events with the id of 1 and display 
+    // that specific data
+
+// ex: {
+//     path: '/events/:eventId',  // :eventId is a URL parameter
+//     element: <EventDetailPage />
+// }
   const params = useParams();
+
+
+
+// create var event; take the EVENTS list in EventsPage.js .find looks through all the events in the list
+    // takes in a parameter named 'event'(we can name this parameter whatever we want)
+    // then compares the event id in the list to the params.eventId
+    // if they are the same then return it 
+  const event = EVENTS.find((event) => event.id === params.eventId);
+
   return (
     <>
       <h1>EventsDetailPage</h1>
       <p>{params.eventId}</p>
+      <h2>Details:</h2>
+      <p>{event.details}</p>
     </>
   );
 }
--- frontend_backend_practice/frontend/src/components/EventsPage.js
+++ frontend_backend_practice/frontend/src/components/EventsPage.js
@@ -1,10 +1,10 @@
 import { Link } from "react-router-dom";
 
 export const EVENTS = [
-  { id: "e1", title: "Event 1", detail: 'January 1, 2020' },
-  { id: "e2", title: "Event 2" },
-  { id: "e3", title: "Event 3" },
-  { id: "e4", title: "Event 4" },
+  { id: "e1", title: "Event 1", details: 'December 31, 2028. Will be lots of fun! ' },
+  { id: "e2", title: "Event 2", details: 'January 1, 2029. Please come!' },
+  { id: "e3", title: "Event 3", details: 'April 8, 2039, Cant miss out!' },
+  { id: "e4", title: "Event 4", details: 'October 29, 3040. Its a birthday celebration! 1,044 years old! '},
 ];
 
 function EventsPage() {
